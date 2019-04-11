7b33f956471c8d871aa51e399afb19cacc6f289b
Jorge Donoso <jdevcl@gmail.com>
2019-04-10 20:13:58 -0400

    Add logic to get image sources

--- src/views/International.vue
+++ src/views/International.vue
@@ -1,27 +1,43 @@
 <template>
   <div>
     <h1>International</h1>
-    <ul>
-      <li v-for="country in International" :key="country.id">
-        <router-link :to="'/international/' + country.fields.countryName">{{country.fields.countryName}}</router-link>
-      </li>
-    </ul>
+    <div
+      v-for="country in internationalData"
+      :key="country.id"
+    >
+      <router-link :to="'/international/' + country.fields.countryName">
+        <h2>{{country.fields.countryName}}</h2>
+        <img
+          :src="findImageSrcById(country.fields.cover.sys.id)"
+          :alt="'photo in '+country.fields.countryName"
+        >
+      </router-link>
+
+    </div>
   </div>
 </template>
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'USA',
-  data: function(){
+  name: "USA",
+  data: function() {
     return {
-      International: []
-    }
+      internationalData: [] as any[],
+      images: [] as any[]
+    };
   },
-  created(){
-    this.$store.dispatch("GET_INTERNATIONAL_DATA").then((res) => {
-      this.International = res.data.items;
+  created() {
+    this.$store.dispatch("GET_INTERNATIONAL_DATA").then(res => {
+      this.internationalData = res.data.items;
+      this.images = res.data.includes.Asset;
     });
+  },
+  methods: {
+    findImageSrcById(id: number): string {
+      const url = this.images.filter(image => image.sys.id === id);
+      return url[0].fields.file.url + "?w=768";
+    }
   }
 });
 </script>
