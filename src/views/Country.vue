a2b0d59c79a5d396e9ad16e7f58a777c5c7d68d9
Jorge Donoso <jdevcl@gmail.com>
2019-04-10 21:57:37 -0400

    Render images for current country

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>{{ $route.params.country }}</h1>
+  <div>
+    <h1>{{ capitalize($route.params.country) }}</h1>
+    <router-link
+      to="/international"
+      exact
+    >Go Back</router-link>
+    <img
+      v-for="photo in imagesForCountry"
+      :key="photo.sys.id"
+      :src="findImageSrcById(photo.sys.id,images,1420)"
+      :alt="'Visiting ' + $route.params.country"
+    >
+  </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      countries: [] as any[],
+      imagesForCountry: [] as any[],
+      images: [] as any[]
+    };
+  },
+  created() {
+    this.$store.dispatch("GET_INTERNATIONAL_DATA").then(response => {
+      this.countries = response.data.items;
+      this.images = response.data.includes.Asset;
+      this.checkValidCountry(this.$route.params.country);
+    });
+  },
+  methods: {
+    checkValidCountry(countryFromParams: string) {
+      // Search all countries for current country.
+      let results = this.countries.filter(
+        country =>
+          country.fields.countryName.toLowerCase() ===
+          countryFromParams.toLowerCase()
+      );
+
+      // If country isn't found, then go back
+      // else grab subset of photos for current country.
+      results.length === 0
+        ? this.$router.push({ path: "/international" })
+        : (this.imagesForCountry = results[0].fields.photos);
+    }
+  }
+});
+</script>
