678832f8929893093f35626e83d17b26e9320edf
Jorge Donoso <jdevcl@gmail.com>
2019-04-11 21:48:34 -0400

    Add Bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import '@/logic/GlobalHelpers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
