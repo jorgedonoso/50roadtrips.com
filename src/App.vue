80f9970865f7c035d64f19d9545d01a2420b9334
Jorge Donoso <jdevcl@gmail.com>
2019-04-11 21:52:16 -0400

    Add basic navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> | 
-      <router-link to="/united-states">United States</router-link> | 
-      <router-link to="/international">International</router-link>
+  <div
+    id="app"
+    class="container"
+  >
+    <div class="row">
+      <div class="col-12">
+        <ul class="nav justify-content-center my-5">
+          <li
+            class="nav-item"
+            v-for="(item, index) in menu"
+            :key="index"
+          >
+            <router-link
+              :to="item.to"
+              class="nav-link"
+            >{{item.label}}</router-link>
+          </li>
+
+        </ul>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
+
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      menu: [
+        { label: "Home", to: "/" },
+        { label: "United States", to: "/united-states" },
+        { label: "International", to: "/international" }
+      ]
+    };
+  }
+});
+</script>
