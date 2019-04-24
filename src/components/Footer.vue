dd70d557aa1e3fac9fb8bf9421b873ac2ed718ba
Jorge Donoso <jdevcl@gmail.com>
2019-04-23 21:56:13 -0400

    Add dynamic footer year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,19 @@
 <template>
   <footer class="text-center m-5">
-    50roadtrips.com &copy; 2019
+    50roadtrips.com &copy; {{year}}
   </footer>
 </template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      year: 0 as number
+    };
+  },
+  created: function() {
+    this.year = new Date().getFullYear();
+  }
+});
+</script>
