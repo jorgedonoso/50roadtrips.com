d7480dd16c2175152b430799e59d1f018f00ab08
Jorge Donoso <jdevcl@gmail.com>
2020-01-12 20:53:37 -0500

    Add Home page base links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <h1>Home</h1>
+  <div class="row">
+    <div class="col">
+      <h1 class="display-1">50 Roadtrips</h1>
+      <router-link
+        :to="item.to"
+        v-for="(item, index) in menu"
+        :key="index"
+        class="btn btn-block btn-primary"
+      >{{item.label}}</router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'home'
+  name: "home",
+  data() {
+    return {
+      menu: [
+        { label: "United States", to: "/united-states" },
+        { label: "International", to: "/international" }
+      ]
+    };
+  }
 });
 </script>
