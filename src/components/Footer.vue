943b805e66dc44c2fd265b623426d6a09e5e5b9a
Jorge Donoso <jdevcl@gmail.com>
2020-01-14 18:55:32 -0500

    Add Contentful to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,29 @@
 <template>
   <footer class="text-center m-5">
     50roadtrips.com &copy; {{year}}
+    <a
+      href="https://www.contentful.com/"
+      rel="nofollow"
+      target="“_blank”"
+      class="d-block mt-2"
+    ><img
+        src="https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
+        alt="Powered by Contentful"
+        style="max-width: 100px; width: 100%;"
+      ></a>
   </footer>
 </template>
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  data: function() {
+  data() {
     return {
-      year: 0 as number
+      year: 0 as number,
     };
   },
-  created: function() {
+  created() {
     this.year = new Date().getFullYear();
-  }
+  },
 });
 </script>
