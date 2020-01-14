52b371a733b4801607fc4273d030136703b9ca26
Jorge Donoso <jdevcl@gmail.com>
2020-01-13 22:58:06 -0500

    Rename mixin

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '@/logic/GlobalHelpers';
+import '@/logic/mixin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
