48b9f252d5dbe29e638ccbf2c7f5511b1c731cfd
Jorge Donoso <jdevcl@gmail.com>
2020-01-14 20:48:51 -0500

    Add more columns to locations table

--- src/components/LocationsTable.vue
+++ src/components/LocationsTable.vue
@@ -2,9 +2,13 @@
   <table class="table table-hover table-striped">
     <thead>
       <tr>
-        <th>Title</th>
         <th>State</th>
         <th>City</th>
+        <th>Title</th>
+        <th>Lon</th>
+        <th>Lat</th>
+        <th>Map</th>
+        <th>Image</th>
       </tr>
     </thead>
     <tbody>
@@ -12,21 +16,32 @@
         v-for="location in data"
         :key="location.id"
       >
-        <td>{{location.fields.title}}</td>
         <td>{{location.fields.state}}</td>
         <td>{{location.fields.city}}</td>
+        <td>{{location.fields.title}}</td>
+        <td>{{location.fields.coordinates.lon}}</td>
+        <td>{{location.fields.coordinates.lat}}</td>
+        <td>
+          <a
+            :href="'https://www.google.com/maps/place/'+location.fields.coordinates.lat+','+location.fields.coordinates.lon"
+            target="_blank"
+          >Map</a>
+        </td>
+        <td>
+          <router-link :to="'/united-states/' + location.fields.photo.sys.id">Image</router-link>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({
-  name: "LocationsTable",
+  name: 'LocationsTable',
   props: {
-    data: Array
-  }
+    data: Array,
+  },
 });
 </script>
