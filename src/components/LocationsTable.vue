c585a54de444c70156834eab363761f36fd31a59
Jorge Donoso <jdevcl@gmail.com>
2020-03-18 14:58:31 -0400

    Combine City and State into one cell for mobile view

--- src/components/LocationsTable.vue
+++ src/components/LocationsTable.vue
@@ -2,7 +2,8 @@
   <table class="table table-hover table-striped">
     <thead>
       <tr>
-        <th>State</th>
+        <th class="d-table-cell d-md-none">Location</th>
+        <th class="d-none d-md-table-cell">State</th>
         <th class="d-none d-md-table-cell">City</th>
         <th>Title</th>
         <th>Photo</th>
@@ -13,7 +14,8 @@
         v-for="location in data"
         :key="location.id"
       >
-        <td>{{location.fields.state}}</td>
+        <td class="d-table-cell d-md-none">{{location.fields.city}}, {{location.fields.state}}</td>
+        <td class="d-none d-md-table-cell">{{location.fields.state}}</td>
         <td class="d-none d-md-table-cell">{{location.fields.city}}</td>
         <td>{{location.fields.title}}</td>
         <td>
--- src/views/USA.vue
+++ src/views/USA.vue
@@ -1,34 +1,34 @@
 <template>
-    <Page>
-        <div class="row">
-            <div class="col-12 mb-4">
-                <Title>United States</Title>
-                <GmapMap
-                    :center="{ lat: 39.876916, lng: -101.351749 }"
-                    :zoom="4"
-                    style="width: 100%; height: 500px"
-                    map-type-id="hybrid"
-                    :labels="true"
-                >
-                    <GmapMarker
-                        :key="index"
-                        v-for="(m, index) in markers"
-                        :position="m.position"
-                        :clickable="true"
-                        :draggable="false"
-                        :animation="2"
-                        @click="handleClick(m.photoId)"
-                    />
-                </GmapMap>
-            </div>
-            <div class="col-12">
-                <SubTitle>Locations</SubTitle>
-                <div class="container">
-                    <locations-table :data="USA" />
-                </div>
-            </div>
+  <Page>
+    <div class="row">
+      <div class="col-12 mb-4">
+        <Title>United States</Title>
+        <GmapMap
+          :center="{ lat: 39.876916, lng: -101.351749 }"
+          :zoom="4"
+          style="width: 100%; height: 500px"
+          map-type-id="hybrid"
+          :labels="true"
+        >
+          <GmapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="false"
+            :animation="2"
+            @click="handleClick(m.photoId)"
+          />
+        </GmapMap>
+      </div>
+      <div class="col-12">
+        <SubTitle>Locations</SubTitle>
+        <div class="container">
+          <locations-table :data="USA" />
         </div>
-    </Page>
+      </div>
+    </div>
+  </Page>
 </template>
 
 <script lang="ts">
@@ -36,40 +36,40 @@ import Vue from 'vue';
 import Page from '@/layouts/Page.vue';
 import LocationsTable from '@/components/LocationsTable.vue';
 import Title from '@/components/Title.vue';
-import SubTitle from '@/components/SubTitle';
+import SubTitle from '@/components/SubTitle.vue';
 import Marker from '@/models/Marker';
 
 export default Vue.extend({
-    components: {
-        LocationsTable,
-        Page,
-        Title,
-        SubTitle,
+  components: {
+    LocationsTable,
+    Page,
+    Title,
+    SubTitle,
+  },
+  data() {
+    return {
+      USA: [],
+      markers: [] as Marker[],
+    };
+  },
+  created() {
+    this.$store.dispatch('GET_USA_DATA').then((res) => {
+      this.USA = res.data.items;
+      res.data.items.forEach((element: any) => {
+        this.markers.push(
+          new Marker(
+            element.fields.coordinates.lat,
+            element.fields.coordinates.lon,
+            element.fields.photo.sys.id,
+          ),
+        );
+      });
+    });
+  },
+  methods: {
+    handleClick(id: any) {
+      this.$router.push('/united-states/' + id);
     },
-    data() {
-        return {
-            USA: [],
-            markers: [] as Marker[],
-        };
-    },
-    created() {
-        this.$store.dispatch('GET_USA_DATA').then((res) => {
-            this.USA = res.data.items;
-            res.data.items.forEach((element: any) => {
-                this.markers.push(
-                    new Marker(
-                        element.fields.coordinates.lat,
-                        element.fields.coordinates.lon,
-                        element.fields.photo.sys.id,
-                    ),
-                );
-            });
-        });
-    },
-    methods: {
-        handleClick(id: any) {
-            this.$router.push('/united-states/' + id);
-        },
-    },
+  },
 });
 </script>
